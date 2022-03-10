4c9e35095f5e8c005a934f500d238864e45ad89e
Semyon Zasov <semenmaslina@gmail.com>
2022-03-10 22:57:16 +0300

    добавил ленту статей

--- src/App.js
+++ src/App.js
@@ -1,15 +1,79 @@
+import React from "react";
 import Menu from "./components/menu/Menu.jsx";
 import Navbar from "./components/navbar/Navbar.jsx";
 import Main from "./pages/main/Main.jsx";
-import Create from "./pages/create/Create.jsx"
-import Registration from "./pages/modal/registration/Registration.jsx"
-import Authorization from "./pages/modal/аuthorization/Authorization.jsx"
+import Create from "./pages/create/Create.jsx";
+import Registration from "./pages/modal/registration/Registration.jsx";
+import Authorization from "./pages/modal/аuthorization/Authorization.jsx";
+
+import { instance } from "./config/axios";
+
+async function post() {
+  // .post("http://localhost:5656/auth/register", {
+  //     "fullName": "Dima Pypkin",
+  //     "email": "wdawdwadaD@dawdawdaw.ru",
+  //     "password": "123456789"
+  // })
+  // await axios.get(`http://localhost:5656/users`);
+  // await axios.get(`http://localhost:5656/posts`);
+  //   await axios.post(
+  //     `http://localhost:5656/posts`,
+  //     {
+  //       title: "Заголовок статьи",
+  //       text: "........",
+  //     },
+  //     {
+  //       headers: {
+  //         Authorization: localStorage.getItem("token"),
+  //       },
+  //     }
+  //   );
+}
 
 function App() {
+  async function reg() {
+    await instance
+      .post("/auth/login", {
+        email: "test@test.ru",
+        password: "Qwerty123",
+      })
+      .then((response) => {
+        const token = response.data.token;
+        localStorage.setItem("token", token);
+      });
+  }
+
+  async function prov() {
+    try {
+      await instance.get("/auth/me");
+      console.log(localStorage.getItem("token"));
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async function create() {
+    try {
+      await instance.post("/posts", {
+        title: "Amet molestie tincidunt id nascetur sit purus turpis",
+        text: "........",
+        description:"Vel vulputate mauris enim habitant ornare. Ut in sit purus turpis ultrices suspendisse scelerisque quam lorem. Amet molestie nascetur...",
+        photoUrl:"https://i.ibb.co/dQWNtRN/post1Png.jpg"
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <>
+      {/* <div>
+        <button onClick={() => reg()}>РЕГ</button>
+        <button onClick={() => prov()}>ПРОВ</button>
+        <button onClick={() => create()}>СОЗ</button>
+      </div> */}
       <Menu></Menu>
       <Navbar></Navbar>
+      <Main></Main>
     </>
   );
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,16 +1,37 @@
-import "./navbar.scss";
+import s from "./navbar.module.scss";
 import searchIcon from "./../../assets/png/search.png";
 import userIcon from "./../../assets/png/user.png";
 
 const Navbar = () => {
   return (
     <>
-      <div className="navbar">
-        <div className="navbar__content">
-          <div className="navbar__header">VASYA BLOG</div>
-          <div className="menu__icon">
-            <img src={searchIcon} className="menu__searchIcon"></img>
-            <img src={userIcon} className="menu__userIcon"></img>
+      <div className={s.navbar}>
+        <div className={s.navbar__content}>
+          <div className={s.navbar__header}>VASYA BLOG</div>
+          <div className={s.menu__icon}>
+            <img src={searchIcon} className={s.menu__searchIcon}></img>
+            <img src={userIcon} className={s.menu__userIcon}></img>
+          </div>
+        </div>
+        <div className={s.article}>
+          <div className={s.article__content}>
+            <div className={s.article__textContent}>
+              <div className={s.article__header}>
+                JavaScript: Как с помощью Data определить город по IP?
+              </div>
+              <div className={s.article__discription}>
+                На работе потребовалось запилить задачу для автоматического
+                определения города при совершении заказа. Было решено сделать
+                это на фронте, ибо бек был занят.
+              </div>
+              <div className={s.article__date}>12 августа 2019 в 08:06</div>
+            </div>
+            <div className={s.article__imgContent}>
+              <img
+                className={s.article__img}
+                src="https://i.ibb.co/xgt0xX2/postPng0.png"
+              />
+            </div>
           </div>
         </div>
       </div>
