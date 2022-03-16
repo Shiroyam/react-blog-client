6e741201a43ce6ee0ebc43753cc9809d7cebd785
Semyon Zasov <semenmaslina@gmail.com>
2022-03-16 16:43:45 +0300

    сделал условный рендер контента меню

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -4,7 +4,8 @@ import { openProfile, closeProfile } from "../../redux/profile/action";
 import closeSearchPng from "./../../assets/png/VectorcloseSerach.png";
 import menuIcon from "./../../assets/png/menuIcon.png";
 import "./menu.scss";
-import { NavLink } from "react-router-dom";
+import { openForm } from "../../redux/regModal/action";
+import { Link, NavLink } from "react-router-dom";
 
 const Menu = () => {
   const [openCloseMenu, setOpenCloseMenu] = React.useState(false);
@@ -12,53 +13,84 @@ const Menu = () => {
 
   const clearLocalStorage = () => {
     localStorage.clear();
+    document.location.reload();
   };
-  
+
   return (
     <>
       <div className="menu">
         {openCloseMenu ? (
           <div className="menuOpen">
-            <div className="menuOpen__header">
-              <div className="menuOpen__name">Вася Пупкин</div>
-              <div className="menuOpen__date">
-                Дата регистрации: 12 августа 2019 в 08:06
+            {localStorage.getItem("token") ? (
+              <>
+                <div className="menuOpen__header">
+                  <div className="menuOpen__name">Вася Пупкин</div>
+                  <div className="menuOpen__date">
+                    Дата регистрации: 12 августа 2019 в 08:06
+                  </div>
+                </div>
+                <div className="menuOpen__nav">
+                  <NavLink
+                    onClick={() => dispatch(closeProfile())}
+                    className="menuOpen__main"
+                    style={{ textDecoration: "none" }}
+                    to="/"
+                  >
+                    Главная
+                  </NavLink>
+                  <NavLink
+                    onClick={() => dispatch(openProfile())}
+                    className="menuOpen__myProfile"
+                    style={{ textDecoration: "none" }}
+                    to="/profile"
+                  >
+                    Мой профиль
+                  </NavLink>
+                  <NavLink
+                    onClick={() => dispatch(closeProfile())}
+                    className="menuOpen__createPage"
+                    style={{ textDecoration: "none" }}
+                    to="/createPage"
+                  >
+                    Создать запись
+                  </NavLink>
+                  <div
+                    onClick={clearLocalStorage}
+                    className="menuOpen__exit"
+                    style={{ textDecoration: "none" }}
+                  >
+                    Выйти
+                  </div>
+                </div>
+              </>
+            ) : (
+              <div className="menuOpen__nav">
+                <NavLink
+                  onClick={() => dispatch(closeProfile())}
+                  className="menuOpen__main"
+                  style={{ textDecoration: "none" }}
+                  to="/"
+                >
+                  Главная
+                </NavLink>
+                <NavLink
+                  onClick={() => dispatch(closeProfile())}
+                  className="menuOpen__main"
+                  style={{ textDecoration: "none" }}
+                  to="/2"
+                >
+                  Зарегистрировать?
+                </NavLink>
+                <div
+                  onClick={()=>dispatch(openForm())}
+                  className="menuOpen__main"
+                  style={{ textDecoration: "none" }}
+                  to="/1"
+                >
+                  Войти
+                </div>
               </div>
-            </div>
-            <div className="menuOpen__nav">
-              <NavLink
-                onClick={()=>dispatch(closeProfile())}
-                className="menuOpen__main"
-                style={{ textDecoration: "none" }}
-                to="/"
-              >
-                Главная
-              </NavLink>
-              <NavLink
-                onClick={() => dispatch(openProfile())}
-                className="menuOpen__myProfile"
-                style={{ textDecoration: "none" }}
-                to="/profile"
-              >
-                Мой профиль
-              </NavLink>
-              <NavLink
-                onClick={()=>dispatch(closeProfile())}
-                className="menuOpen__createPage"
-                style={{ textDecoration: "none" }}
-                to="/createPage"
-              >
-                Создать запись
-              </NavLink>
-              <div
-                onClick={clearLocalStorage}
-                className="menuOpen__exit"
-                style={{ textDecoration: "none" }}
-                to="/createPage"
-              >
-                Выйти
-              </div>
-            </div>
+            )}
             <div className="menuOpen__closeMenu">
               <div className="menuOpen__content">
                 <img
